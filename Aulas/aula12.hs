08d0d3a4c5b25cf1eba34e794b208929aeb9bb32
Matheus Vicente <matt.vicent@gmail.com>
2019-06-04 12:37:00 -0300

    Add files via upload

--- Aulas/aula12.hs
+++ Aulas/aula12.hs
@@ -1,3 +1,6 @@
+import Data.Maybe
+import Data.List
+
 data Nat = Zero | Succ Nat deriving Show
 
 -- naturalParaInteiro' a n = case a of
@@ -57,7 +60,7 @@ operatorSize (Mult a b) = 1 + operatorSize a + operatorSize b
 1       3       5      7
 --------------------------}
 
-data Arvore a = Vazia | No (Arvore a) a (Arvore a)
+data Arvore a = Vazia | No (Arvore a) a (Arvore a) deriving (Show, Eq)
 
 arvoreTeste = No (No (No Vazia 1 Vazia) 2 (No Vazia 3 Vazia)) 4 (No (No Vazia 5 Vazia) 6 (No Vazia 7 Vazia))
 
@@ -81,3 +84,59 @@ buscaBinaria x (No esq meio dir) | x == meio = True
 
 buscaBinariaTalvez x arv | buscaBinaria x arv = Just x
                          | otherwise = Nothing
+
+
+inserir Vazia elemento = No Vazia elemento Vazia
+inserir (No a1 a a2) elemento |  a == elemento = No a1 a a2 
+                              |  a > elemento = No (inserir a1 elemento) a a2
+                              |  otherwise = No a1 a (inserir a2 elemento)
+
+
+
+
+
+
+-- remover (No Vazia a Vazia) = Vazia
+-- remover (No Vazia a dir) = dir
+-- remover (No esq a Vazia) = esq
+
+remove x Vazia = Vazia
+remover x (No esq a Vazia) | x == a = esq
+remover x (No Vazia a dir) | x == a = dir
+remover x (No esq a dir) | x == a = No (remover b esq) b (remover b dir) 
+                         | otherwise =  No (remover x esq) a (remover x dir) 
+        where
+            succ = sucessor (No esq a dir)
+            No e b d = succ
+
+menor Vazia = Vazia
+menor (No esq _ _) | menor esq == Vazia = esq
+                   | otherwise = menor esq
+
+sucessor Vazia = Vazia
+sucessor (No _ _ dir) = menor dir 
+
+
+
+emOrdem (Vazia) = []
+emOrdem (No esq a dir) = emOrdem esq ++ [a] ++ emOrdem dir
+
+
+preOrdem (Vazia) = []
+preOrdem (No esq a dir) = [a] ++ preOrdem esq ++ preOrdem dir
+
+
+posOrdem (Vazia) = []
+posOrdem (No esq a dir) = posOrdem esq ++ posOrdem dir ++ [a]
+
+
+criarArvore [] = Vazia
+criarArvore [x] = No Vazia x Vazia 
+criarArvore lista = No (criarArvore esquerda) mediana (criarArvore direita)
+    where 
+        listaOrdenada = sort lista
+        medianaIndice = (length lista) `div` 2
+        esquerda = take (medianaIndice) listaOrdenada
+        direita = drop (medianaIndice) listaOrdenada
+        mediana = listaOrdenada !! medianaIndice
+
